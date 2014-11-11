fa4f8037dc793852420cc8339756b60ae74e801d
Roma <chrisroma23@gmail.com>
2014-11-10 20:13:30 -0500

    got a little more file IO

--- FileIO.java
+++ FileIO.java
@@ -1,9 +1,12 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Formatter;
 
 import javax.swing.JOptionPane;
 
 
 public class FileIO {
+	Formatter formatter;
 	private UserInfo userObj = new UserInfo();
 	
 	public void setNewUser(UserInfo userObj){
@@ -17,7 +20,12 @@ public class FileIO {
 		if(userFile.exists()){
 			JOptionPane.showMessageDialog(null, userFile.getName() + " Exists");
 		}else{
-			JOptionPane.showMessageDialog(null, userFile.getName() + " needs to be created");
+			try{
+				formatter = new Formatter(userFile.getName());
+			}catch(FileNotFoundException e){
+				e.printStackTrace();
+			}
+			JOptionPane.showMessageDialog(null, userFile.getName() + " was created");
 		}
 		
 	}
